c17d108940dadfff9cbe608fcc865aafb6ccff32
TeddTech <ted_thompson@live.com>
2019-12-18 19:24:36 -0800

    #2 endpoint variables

---
+++ we_count/assets/config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const Config = {
+	appTitle: "we_count",
+	appTitleShort: "WC",
+	appDescription:
+		"We Count Project",
+	appThemeColor: "#fffffff ",
+	appBgColor: "#00172c ",
+	appIcon: "assets/app-icon.png",
+	wpDomain: "https://wecount.inclusivedesign.ca",
+	loadDbName: "starter_wp",
+	api: {
+		destinations: ""
+	}
+
+};
+
+module.exports = Config;
