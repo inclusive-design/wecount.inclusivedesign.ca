17087a0d773f78c7338587450f79413d1415d8b6
Tedd Tech <ted_thompson@live.com>
2020-02-05 15:00:39 -0800

    Next not defined (fix #27) (#30)
    * #27 fixed linting error: 'next' is not defined
    
    next is an Express.js command
    fixed error by removing next
    
    * added callback to catch
    
    now catching errors according to nuxt docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,9 +35,8 @@ export default {
 					})
 
 					callback(null, pageRoutes.concat(postRoutes))
-				}), function (err) {
-					return next(err)
-				})
+				}))
+				.catch(callback)
 		}
 	},
 	/*
