937313eea14a4862f44508b21424a9944e01df49
TeddTech <ted_thompson@live.com>
2020-02-11 10:17:07 -0800

    fix: remove bootstrap vue
    resoves #49

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,7 +67,6 @@ export default {
 	** Plugins to load before mounting the App
 	*/
 	plugins: [
-		"@/plugins/bootstrap-vue"
 	],
 	/*
 	** Nuxt.js dev-modules
@@ -80,8 +79,6 @@ export default {
 	** Nuxt.js modules
 	*/
 	modules: [
-		// Doc: https://bootstrap-vue.js.org
-		"bootstrap-vue/nuxt",
 		// Doc: https://axios.nuxtjs.org/usage
 		"@nuxtjs/axios",
 		"@nuxtjs/pwa",
