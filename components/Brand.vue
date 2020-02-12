a075a577227ce353397eb0bebfbbd3cc531a01b1
TeddTech <ted_thompson@live.com>
2020-02-12 13:58:01 -0800

    fix: refractored brand and added alt property

--- components/Brand.vue
+++ components/Brand.vue
@@ -1,20 +1,8 @@
 <template>
 	<div class="brand">
-		<img src="~/static/img/We Count logos_colour and bw-01.png">
-		<h1> We Count </h1>
+		<nuxt-link to="/">
+			<img src="~/static/img/We Count logos_colour and bw-01.png" alt="Website Logo">
+			We Count
+		</nuxt-link>
 	</div>
 </template>
-
-<style scoped>
-img {
-	max-width: 10%;
-}
-.brand {
-	display: flex;
-	flex-flow: row;
-}
-
-h1 {
-	margin:  3rem 0;
-}
-</style>
