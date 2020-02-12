dae5f1b27beab76f0a9d3b194b493d7e69b6ebd7
TeddTech <ted_thompson@live.com>
2020-02-12 14:11:41 -0800

    fix: edited alt tags

--- components/Brand.vue
+++ components/Brand.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="brand">
 		<nuxt-link to="/">
-			<img src="~/static/img/We Count logos_colour and bw-01.png" alt="Website Logo">
+			<img src="~/static/img/We Count logos_colour and bw-01.png" alt="Logo for Project We Count">
 			We Count
 		</nuxt-link>
 	</div>
