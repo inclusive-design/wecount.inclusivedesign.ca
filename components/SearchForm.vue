f5eba078ea3dd4a2a31e0942d14fc14fa8984514
TeddTech <ted_thompson@live.com>
2020-02-12 15:12:32 -0800

    feat: ran npm run lint

--- components/SearchForm.vue
+++ components/SearchForm.vue
@@ -1,8 +1,10 @@
 <template>
 	<div class="search-container">
-			<form action="/action_page.php">
-				<button type="submit"><i class="fa fa-search"></i></button>
-				<input type="text" placeholder="Search..." name="search">
-			</form>
+		<form action="/action_page.php">
+			<button type="submit">
+				<i class="fa fa-search" />
+			</button>
+			<input type="text" placeholder="Search..." name="search">
+		</form>
 	</div>
 </template>
