c0c1b5f1b2924ba19c52a574094ab3e2fcc4082b
TeddTech <ted_thompson@live.com>
2020-02-12 14:55:18 -0800

    feat: added component header search
    resolves #42

---
+++ components/SearchForm.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="search-container">
+			<form action="/action_page.php">
+				<button type="submit"><i class="fa fa-search"></i></button>
+				<input type="text" placeholder="Search..." name="search">
+			</form>
+	</div>
+</template>
