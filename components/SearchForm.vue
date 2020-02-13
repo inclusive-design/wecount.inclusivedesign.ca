c1fc745e22adf855bce9838255f1217f1b413a84
Tedd Tech <ted_thompson@live.com>
2020-02-13 11:00:16 -0800

    Component header search (#54)
    * feat: added component header search
    resolves #42
    * fix: corrected linting errors and added label

---
+++ components/SearchForm.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="search-container">
+		<form id="search-form" action="">
+			<label for="search">Search for:</label>
+			<input id="search" type="search" placeholder="Search..." name="search">
+			<button type="submit">
+				Search
+			</button>
+		</form>
+	</div>
+</template>
