55380178a293859e7f8d576696fd533c9d830811
TeddTech <ted_thompson@live.com>
2020-02-13 09:20:47 -0800

    feat: added component main article
    resolves #46

---
+++ components/Article.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h1 class="title">
+			<b>{{ Title }}</b>
+		</h1>
+		<p id="api-content" v-html="Content" />
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		Title: {
+			type: String,
+			default: "Untitled Post"
+		},
+		Content: {
+			type: String,
+			default: "We Count content"
+		}
+	}
+}
+</script>
