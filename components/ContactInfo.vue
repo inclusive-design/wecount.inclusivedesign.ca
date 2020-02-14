b53604d634e28de1bdd4d40c4eb75e9c18a49abf
TeddTech <ted_thompson@live.com>
2020-02-13 23:40:24 -0800

    fix: ran npm run lint:fix

--- components/ContactInfo.vue
+++ components/ContactInfo.vue
@@ -1,11 +1,13 @@
 <template>
-	<div><p>
-	Contact Us
-	</p>
-	<p>
-	email@wecount.ca
-	IDRC
-	205 Richmond Street West
-	<img src="~/static/img/idrc.png" />
-	</p></div>
+	<div>
+		<p>
+			Contact Us
+		</p>
+		<p>
+			email@wecount.ca
+			IDRC
+			205 Richmond Street West
+			<img src="~/static/img/idrc.png">
+		</p>
+	</div>
 </template>
