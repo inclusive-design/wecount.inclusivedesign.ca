cdd7dfb0f00b06de20c52216045bd4a1ad50e36b
TeddTech <ted_thompson@live.com>
2020-02-13 23:54:30 -0800

    feat: added social media component
    resolves #44

--- components/SocialMedia.vue
+++ components/SocialMedia.vue
@@ -1,1 +1,17 @@
-<template><div /></template>
+<template>
+	<div>
+		<p>Follow us on social media</p>
+		<nuxt-link to="">
+			<img src="">
+		</nuxt-link>
+		<nuxt-link to="">
+			<img src="">
+		</nuxt-link>
+		<nuxt-link to="">
+			<img src="">
+		</nuxt-link>
+		<nuxt-link to="">
+			<img src="">
+		</nuxt-link>
+	</div>
+</template>
