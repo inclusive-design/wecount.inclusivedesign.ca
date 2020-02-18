3255150fec30831b7305c2b2c9caa5c2a859fa97
TeddTech <ted_thompson@live.com>
2020-02-18 11:30:03 -0800

    feat: added news grid component
    resolves #69

---
+++ components/NewsGrid.vue
@@ -0,0 +1,49 @@
+
+<template>
+	<article class="news-grid">
+		<div v-for="row in groupedList" :key="row.id" class="row">
+			<div v-for="x in row" :key="x.id" class="api-content">
+				<NewsItem
+					v-if="row.length>1"
+					:picture="x.picture"
+					:title="x.title"
+					:date="x.date"
+					:slug="x.slug"
+				/>
+				<NewsItem
+					v-else
+					:picture="x.picture"
+					:title="x.title"
+					:date="x.date"
+					:slug="x.slug"
+					style="width: 50%"
+				/>
+			</div>
+		</div>
+	</article>
+</template>
+
+<script>
+import _ from "lodash"
+import NewsItem from "~/components/NewsItem"
+export default {
+	components: {
+		NewsItem
+	},
+	props: {
+		title: {
+			type: String,
+			default: "Untitled"
+		},
+		postList: {
+			type: String,
+			default: () => []
+		}
+	},
+	computed: {
+		groupedList () {
+			return _.chunk(this.postList, 2)
+		}
+	}
+}
+</script>
