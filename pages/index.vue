0649e87d99e4ea0a2cffcda0f3f82a634894708a
Tedd Tech <ted_thompson@live.com>
2020-02-18 13:48:37 -0800

    feat: update page templates (#67)
    * feat: Update page templates
    to use Article component
    resolves #62
    * fix: corrected page name type
    * fix: added Article component
    to components method
    * fix: changed replaced out data with our data

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,19 @@
 <template>
-	<b-row align-h="center">
-		<div id="container">
-			<h1 id="title">
-				<b>{{ Title }}</b>
-			</h1>
-			<div id="api-content" v-html="Content" />
-		</div>
-	</b-row>
+	<Article :title="title" :content="content" />
 </template>
 
 <script>
 import axios from "axios"
+import Article from "~/components/Article"
 import Config from "~/assets/config"
 export default {
 	components: {
+		Article
 	},
 	data () {
 		return {
-			Title: "",
-			Content: ""
+			title: "",
+			content: ""
 		}
 	},
 	asyncData (context) {
@@ -26,53 +21,19 @@ export default {
 		if (context.payload) {
 		// extract the page object passed from nuxt.config.js
 			return {
-				Title: context.payload.page.title.rendered.toUpperCase(),
-				Content: context.payload.page.content.rendered
+				title: context.payload.page.title.rendered.toUpperCase(),
+				content: context.payload.page.content.rendered
 			}
 		} else {
 		// if you got no context, go ahead and make the API request
 			return axios.get(`${Config.wpDomain}${Config.apiBase}pages`).then((response) => {
 				const res = response.data.filter(x => x.title.rendered === "We Count")[0]
 				return {
-					Title: res.title.rendered.toUpperCase(),
-					Content: res.content.rendered
+					title: res.title.rendered.toUpperCase(),
+					content: res.content.rendered
 				}
 			})
 		}
 	}
 }
 </script>
-
-<style>
-/* contains unneeded css fields */
-.container {
-	align-items: center;
-	display: flex;
-	justify-content: center;
-	margin: 0 auto;
-	min-height: 100vh;
-	text-align: center;
-}
-
-.title {
-	color: #35495e;
-	display: block;
-	font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-		'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-	font-size: 100px;
-	font-weight: 300;
-	letter-spacing: 1px;
-}
-
-.subtitle {
-	color: #526488;
-	font-size: 42px;
-	font-weight: 300;
-	padding-bottom: 15px;
-	word-spacing: 5px;
-}
-
-.links {
-	padding-top: 15px;
-}
-</style>
