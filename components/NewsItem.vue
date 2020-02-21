f1c7bba91551f7672a7721eb239eda3d6829ff1c
Tedd Tech <ted_thompson@live.com>
2020-02-21 12:27:24 -0800

    feat: added NewsItem component (#68)
    * feat: added NewsItem component
    
    resolves #63
    
    * feat: refractored news page
    
    using NewItem component
    
    resolves #64
    
    * fix: renamed post to NewsItem
    
    * feat: added news grid component
    
    resolves #69
    
    * fix: removed title prop from NewsGrid component
    
    * feat: refractored news page
    
    with news grid component.
    
    * fix: refractored structure of NewsItem component
    
    * fix: changed article tag to div tag
    
    in NewsGrid component
    
    * fix: corrected prop types
    
    removed inline styling from NewsGrid component
    
    refractored figure tag in NewItem component
    
    * fix: removed comments
    
    removed funtion from defalut value in picture prop
    
    * fix:  changes for 2020-02-20 we count check in
    added caption prop to NewsItem component for alt attribute in img tag
    and
    dateTime prop to NewsItem component for datetime attribute time in tag
    
    * fix: changed caption to altTag
    
    * feat: move link into <h2>
    
    Co-authored-by: Ned Zimmerman <ned@bight.dev>

---
+++ components/NewsItem.vue
@@ -0,0 +1,47 @@
+<template>
+	<!-- This compenent is used by data from pages api call as well as post api call -->
+	<article>
+		<figure class="preview-media">
+			<img :src="picture" :alt="altTag" class="news-item-img">
+		</figure>
+		<h2>
+			<nuxt-link :to="'/news-and-views/' + slug" class="news-item">
+				{{ title }}
+			</nuxt-link>
+		</h2>
+		<div class="date">
+			<time :datetime="dateTime">{{ date }}</time>
+		</div>
+	</article>
+</template>
+
+<script>
+export default {
+	props: {
+		picture: {
+			type: String,
+			default: "https://wecount.inclusivedesign.ca/wp-content/uploads/2019/10/We-Count-logos_colour-and-bw-01.png"
+		},
+		altTag: {
+			type: String,
+			default: "post thumbnail"
+		},
+		title: {
+			type: String,
+			default: "Untitled Post"
+		},
+		date: {
+			type: String,
+			default: ""
+		},
+		dateTime: {
+			type: String,
+			default: ""
+		},
+		slug: {
+			type: String,
+			default: ""
+		}
+	}
+}
+</script>
--- store/actions.js
+++ store/actions.js
@@ -10,6 +10,7 @@ export default {
 			const slug = (r.data[i].slug === "home") ? "/" : r.data[i].slug
 			const title = r.data[i].title.rendered
 			const content = r.data[i].content.rendered
+			const dateTime = r.data[i].date
 			const dateOptions = {
 				year: "numeric",
 				month: "long",
@@ -18,15 +19,17 @@ export default {
 			const date = new Date(r.data[i].date).toLocaleString("en-us", dateOptions)
 			let picture = "https://wecount.inclusivedesign.ca/wp-content/uploads/2019/10/We-Count-logos_colour-and-bw-01.png"
 			let tags = []
+			let altTag = ""
 
 			if (postType === "posts") {
 				tags = r.data[i].pure_taxonomies.tags.map(({ name }) => name)
 				const api = r.data[i]._links["wp:featuredmedia"][0].href
 				const pic = await axios.get(api)
 				picture = pic.data.guid.rendered
+				altTag = pic.data.alt_text
 			}
 
-			Results.push({ slug, title, date, picture, content, tags })
+			Results.push({ slug, title, date, picture, altTag, content, tags, dateTime })
 		}
 		if (postType === "posts") {
 			context.commit("REFRESH_POSTS", Results)
