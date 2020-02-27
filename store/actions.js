8a3cb7e36df85d645c5f0ae7c56956adef769576
TeddTech <ted_thompson@live.com>
2020-02-27 12:04:34 -0800

    fix: accounted for paginated api request in actions.js

--- store/actions.js
+++ store/actions.js
@@ -3,45 +3,55 @@ import Config from "~/assets/config"
 
 export default {
 	fetchApiData: async (context, postType) => {
-		const Results = []
-		const r = await axios.get(`${Config.wpDomain}${Config.apiBase}${postType}`)
+		const totalPages = await (await axios.get(`${Config.wpDomain}${Config.apiBase}${postType}`)).headers["x-wp-totalpages"]
+		const totalNestedResults = []
 		let count = 0
 
-		for (let i = 0; i < r.data.length; i++) {
-			const slug = (r.data[i].slug === "home") ? "/" : r.data[i].slug
-			const title = r.data[i].title.rendered
-			const content = r.data[i].content.rendered
-			const dateTime = r.data[i].date
-			const dateOptions = {
-				year: "numeric",
-				month: "long",
-				day: "numeric"
-			}
-			const date = new Date(r.data[i].date).toLocaleString("en-us", dateOptions)
-			let picture = "https://wecount.inclusivedesign.ca/wp-content/uploads/2019/10/We-Count-logos_colour-and-bw-01.png"
-			let tags = []
-			let altTag = ""
+		for (let index = 0; index < totalPages; index++) {
+			const Results = []
+			const r = await axios.get(`${Config.wpDomain}${Config.apiBase}${postType}`)
 
-			if (postType === "posts") {
-				count += 1
-				if (r.data[i].pure_taxonomies.tags !== undefined) {
-					tags = r.data[i].pure_taxonomies.tags.map(({ name }) => name)
+			for (let i = 0; i < r.data.length; i++) {
+				const slug = (r.data[i].slug === "home") ? "/" : r.data[i].slug
+				const title = r.data[i].title.rendered
+				const content = r.data[i].content.rendered
+				const dateTime = r.data[i].date
+				const dateOptions = {
+					year: "numeric",
+					month: "long",
+					day: "numeric"
 				}
-				if (r.data[i]._links["wp:featuredmedia"] !== undefined) {
-					const api = r.data[i]._links["wp:featuredmedia"][0].href
-					const pic = await axios.get(api)
-					picture = pic.data.source_url
-					altTag = pic.data.alt_text
+				const date = new Date(r.data[i].date).toLocaleString("en-us", dateOptions)
+				let picture = "https://wecount.inclusivedesign.ca/wp-content/uploads/2019/10/We-Count-logos_colour-and-bw-01.png"
+				let tags = []
+				let altTag = ""
+
+				if (postType === "posts") {
+					count += 1
+					if (r.data[i].pure_taxonomies.tags !== undefined) {
+						tags = r.data[i].pure_taxonomies.tags.map(({ name }) => name)
+					}
+					if (r.data[i]._links["wp:featuredmedia"] !== undefined) {
+						const api = r.data[i]._links["wp:featuredmedia"][0].href
+						const pic = await axios.get(api)
+						picture = pic.data.source_url
+						altTag = pic.data.alt_text
+					}
 				}
+
+				Results.push({ slug, title, date, picture, altTag, content, tags, dateTime, count })
 			}
 
-			Results.push({ slug, title, date, picture, altTag, content, tags, dateTime, count })
+			totalNestedResults.push(Results)
 		}
+
+		const totalResults = totalNestedResults.flat()
+
 		if (postType === "posts") {
-			context.commit("REFRESH_POSTS", Results)
+			context.commit("REFRESH_POSTS", totalResults)
 		}
 		if (postType === "pages") {
-			context.commit("REFRESH_PAGES", Results)
+			context.commit("REFRESH_PAGES", totalResults)
 		}
 	}
 }
