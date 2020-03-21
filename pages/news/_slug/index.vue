46f89221e15bf7c89946e0135980f3e727e61dcc
TeddTech <ted_thompson@live.com>
2020-03-20 18:57:13 -0700

    feat: add Figma styling to news and views

---
+++ pages/news/_slug/index.vue
@@ -0,0 +1,48 @@
+<template>
+	<PostArticle :title="Title" :content="Content" :picture="Picture" :tags="Tags" />
+</template>
+
+<script>
+import PostArticle from "~/components/PostArticle"
+export default {
+	async validate ({ params, store }) {
+		if (store.state.posts === null) {
+			await store.dispatch("fetchApiData", "posts")
+		}
+		const linkList = store.state.posts.map(({ slug }) => slug)
+		return linkList.includes(params.slug)
+	},
+	head () {
+		return {
+			title: this.Title
+		}
+	},
+	components: {
+		PostArticle
+	},
+	data () {
+		return {
+		}
+	},
+	computed: {
+		links () {
+			return this.$store.state.posts.map(({ slug }) => slug)
+		},
+		linkNames () {
+			return this.links.map(x => x.replace(/-/g, " "))
+		},
+		Title () {
+			return this.$store.state.posts.find(post => post.slug === this.$route.params.slug).title
+		},
+		Picture () {
+			return this.$store.state.posts.find(post => post.slug === this.$route.params.slug).picture
+		},
+		Content () {
+			return this.$store.state.posts.find(post => post.slug === this.$route.params.slug).content
+		},
+		Tags () {
+			return this.$store.state.posts.find(post => post.slug === this.$route.params.slug).tags
+		}
+	}
+}
+</script>
