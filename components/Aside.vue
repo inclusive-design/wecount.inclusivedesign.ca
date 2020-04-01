3fcce14138e1534d4918b19667f106f6c22b505d
TeddTech <ted_thompson@live.com>
2020-03-31 21:56:59 -0700

    fix: styled page content
    resolves #115

--- components/Aside.vue
+++ components/Aside.vue
@@ -1,9 +1,24 @@
 <template>
 	<aside>
 		<ul>
-			<li v-for="h in headers" :key="h.id">
+			<!-- <li v-for="h in headers" :key="h.id">
 				<nuxt-link :to="h.href">
 					{{ h.title }}
+				</nuxt-link>
+			</li> -->
+			<li>
+				<nuxt-link to="#title1">
+					title1
+				</nuxt-link>
+			</li>
+			<li>
+				<nuxt-link to="#title2">
+					title2
+				</nuxt-link>
+			</li>
+			<li>
+				<nuxt-link to="#title3">
+					title3
 				</nuxt-link>
 			</li>
 		</ul>
@@ -11,6 +26,7 @@
 </template>
 
 <script>
+// The above <li> are coded to show an example of how styles will look. This will be replaced by the commented out html code above.
 export default {
 	props: {
 		headers: {
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
 	<div id="defaultContainer">
 		<Header />
+		<Aside :headers="headers" />
 		<nuxt />
 		<Footer />
 	</div>
@@ -8,10 +9,12 @@
 
 <script>
 import Header from "~/components/Header"
+import Aside from "~/components/Aside"
 import Footer from "~/components/Footer"
 export default {
 	components: {
 		Header,
+		Aside,
 		Footer
 	},
 	data () {
