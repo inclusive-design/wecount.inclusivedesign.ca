ff870cf6894f32323bf5a720abe3673f12ffcd4c
Ned Zimmerman <ned@bight.dev>
2020-04-03 16:06:21 -0600

    fix: remove example content in aside

--- components/Aside.vue
+++ components/Aside.vue
@@ -1,26 +1,10 @@
 <template>
 	<aside>
-		<nav>
+		<nav v-if="headers.length > 0">
 			<ul>
-				<!-- <li v-for="h in headers" :key="h.id">
-		<nuxt-link :to="h.href">
-		{{ h.title }}
-		</nuxt-link>
-		</li> -->
-				<!-- TODO: the <li> elements below are hard coded to show an example of how styles will look. This will be replaced by the commented out html code above. -->
-				<li>
-					<nuxt-link to="#title1">
-						title1
-					</nuxt-link>
-				</li>
-				<li>
-					<nuxt-link to="#title2">
-						title2
-					</nuxt-link>
-				</li>
-				<li>
-					<nuxt-link to="#title3">
-						title3
+				<li v-for="h in headers" :key="h.id">
+					<nuxt-link :to="h.href">
+						{{ h.title }}
 					</nuxt-link>
 				</li>
 			</ul>
