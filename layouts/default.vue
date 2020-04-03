8690e891b8cfb23a02528e280ef8eace7d0f9d38
Tedd Tech <ted_thompson@live.com>
2020-04-03 14:43:51 -0700

    Style page content (#134)
    * fix: ran npm install
    
    * fix: styled page content
    
    resolves #115
    
    * fix: table of content text wraps
    
    * fix: moved commented code
    
    * fix: removed headers property from Aside element
    
    * fix: wrapped main element around aside and nuxt element
    
    * feat: use CSS grid for main content area and aside
    
    * fix: remove margin-left
    
    Co-authored-by: Ned Zimmerman <ned@bight.dev>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,24 @@
 <template>
 	<div id="defaultContainer">
 		<Header />
-		<nuxt />
+		<div class="content-wrapper">
+			<Aside />
+			<main>
+				<nuxt />
+			</main>
+		</div>
 		<Footer />
 	</div>
 </template>
 
 <script>
 import Header from "~/components/Header"
+import Aside from "~/components/Aside"
 import Footer from "~/components/Footer"
 export default {
 	components: {
 		Header,
+		Aside,
 		Footer
 	},
 	data () {
