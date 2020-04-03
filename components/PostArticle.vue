16b0e9490097f99ed6d3f563f01cb2b2b68af5d6
Cindy Qi Li <cli@ocadu.ca>
2020-04-03 09:47:36 -0400

    Issue-117: Address review suggestions.

--- components/PostArticle.vue
+++ components/PostArticle.vue
@@ -9,7 +9,7 @@
 		<div v-html="postToRender.content" class="api-content" />
 
 		<section class="tags-info">
-			<p>Active Tags</p>
+			<h3>Active Tags</h3>
 			<div class="tags">
 				<nuxt-link v-for="(t, index) in postToRender.tags" :key="index" :to="{ path: '/tag', query: { s: t }}">
 					{{ t }}
