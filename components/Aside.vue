82a169762e7094f1b867e7b0dc4b9145005101d9
Cindy Qi Li <cli@ocadu.ca>
2020-04-03 17:59:54 -0400

    fix: remove the test data.

--- components/Aside.vue
+++ components/Aside.vue
@@ -2,27 +2,12 @@
 	<aside>
 		<nav>
 			<ul>
+				<!-- TODO: Uncomment the code below when the secondary nav items are available -->
 				<!-- <li v-for="h in headers" :key="h.id">
-		<nuxt-link :to="h.href">
-		{{ h.title }}
-		</nuxt-link>
-		</li> -->
-				<!-- TODO: the <li> elements below are hard coded to show an example of how styles will look. This will be replaced by the commented out html code above. -->
-				<li>
-					<nuxt-link to="#title1">
-						title1
+					<nuxt-link :to="h.href">
+					{{ h.title }}
 					</nuxt-link>
-				</li>
-				<li>
-					<nuxt-link to="#title2">
-						title2
-					</nuxt-link>
-				</li>
-				<li>
-					<nuxt-link to="#title3">
-						title3
-					</nuxt-link>
-				</li>
+					</li> -->
 			</ul>
 		</nav>
 	</aside>
