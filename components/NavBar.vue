6cf0bea9516a3e800fdbfdee0762f4455175d896
TeddTech <ted_thompson@live.com>
2020-04-06 16:56:08 -0700

    fix: using display none instead of deleting elements

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,8 +3,14 @@
 		<nuxt-link to="/about/">
 			About
 		</nuxt-link>
+		<nuxt-link to="/tools/" style="display: none;">
+			Tools
+		</nuxt-link>
 		<nuxt-link to="/inclusion-challenges/">
 			Inclusion Challenges
+		</nuxt-link>
+		<nuxt-link to="/our-data/" style="display: none;">
+			Our Data
 		</nuxt-link>
 		<nuxt-link to="/news/">
 			News
