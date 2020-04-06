9cba37a7cc87b91c952239d3da7d7ac2d252c013
TeddTech <ted_thompson@live.com>
2020-04-06 16:13:48 -0700

    fix: remove tools and our data from navigation

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,14 +3,8 @@
 		<nuxt-link to="/about/">
 			About
 		</nuxt-link>
-		<nuxt-link to="/tools/">
-			Tools
-		</nuxt-link>
 		<nuxt-link to="/inclusion-challenges/">
 			Inclusion Challenges
-		</nuxt-link>
-		<nuxt-link to="/our-data/">
-			Our Data
 		</nuxt-link>
 		<nuxt-link to="/news/">
 			News
