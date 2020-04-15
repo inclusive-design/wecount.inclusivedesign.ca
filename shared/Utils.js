87e41725035fa18001c445ae3de9b504f03377b6
Cindy Qi Li <cli@ocadu.ca>
2020-04-15 10:33:55 -0400

    Issue-170: Address more review comments.

--- shared/Utils.js
+++ shared/Utils.js
@@ -6,11 +6,11 @@ export default {
 	},
 
 	/**
-	 * The complete Triforce, or one or more components of the Triforce.
-	 * @typedef {Object} menuItem
-	 * @property {String} slug - The slug of the corresponding page that the menu item lands on.
-	 * @property {String} title - The title of the corresponding page that the menu item lands on.
-	 * @property {String} href - The href to direct the menu item to its corresponding page.
+	 * generateNavMenu() returns an array of menu items. The structure of a menu item is:
+	 * @typedef {Object} MenuItem
+	 * @property {string} slug - The slug of the corresponding page that the menu item lands on.
+	 * @property {string} title - The title of the corresponding page that the menu item lands on.
+	 * @property {string} href - The href to direct the menu item to its corresponding page.
 	 */
 
 	/**
@@ -18,14 +18,17 @@ export default {
 	 * The retun menu array will:
 	 * 1. Exclude all pages that have menu_order === 0;
 	 * 2. Append "News" and "Views" menu items to the end.
-	 * @param {Array<Object>} sitePages - All menu pages sorted by menu_order.
-	 * @param {Boolean} includeHomeItem - A flag indicating if the home menu item should be included.
-	 * @return {Array<menuItem>} An array of object including the slug, menu title and href.
+	 * @param {array<Object>} sitePages - All menu pages sorted by menu_order.
+	 * @param {boolean} includeHomeItem - A flag indicating if the home menu item should be included.
+	 * @return {array<MenuItem>} An array of object including the slug, menu title and href.
 	 */
 	generateNavMenu (sitePages, includeHomeItem) {
 		const menuItems = [];
 		const homeMenuItem = [];
-		const itemsToAppend = [{
+		// Since "News" and "Views" don't have landing pages defined at the WP back-end, these menu items need to
+		// be explicitly appended to the nav menu. Using "append" is because they are the last 2 nav elements according
+		// to the design.
+		const extraMenuItemsToAppend = [{
 			slug: "news",
 			title: "News",
 			href: "/news"
@@ -45,7 +48,7 @@ export default {
 			}
 
 			// Exclude pages that have menu_order === 0, which indicates the page should not be displayed on the header nav menu.
-			if (oneSitePage.menu_order) {
+			if (oneSitePage.menu_order !== 0) {
 				menuItems.push({
 					slug: oneSitePage.slug,
 					title: oneSitePage.title,
@@ -54,6 +57,6 @@ export default {
 			}
 		});
 
-		return [...homeMenuItem, ...menuItems, ...itemsToAppend];
+		return [...homeMenuItem, ...menuItems, ...extraMenuItemsToAppend];
 	}
 };
