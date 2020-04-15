b7b881efe8c765f133f22a235eb6cb9308ad9f36
Cindy Qi Li <cli@ocadu.ca>
2020-04-15 10:18:01 -0400

    Issue-170: Address review comments.

--- shared/Utils.js
+++ shared/Utils.js
@@ -6,17 +6,23 @@ export default {
 	},
 
 	/**
+	 * The complete Triforce, or one or more components of the Triforce.
+	 * @typedef {Object} menuItem
+	 * @property {String} slug - The slug of the corresponding page that the menu item lands on.
+	 * @property {String} title - The title of the corresponding page that the menu item lands on.
+	 * @property {String} href - The href to direct the menu item to its corresponding page.
+	 */
+
+	/**
 	 * Generate the navigation menu based on the response from the Wordpress API.
 	 * The retun menu array will:
-	 * 1. Exclude all pages whose menu_order === 0;
+	 * 1. Exclude all pages that have menu_order === 0;
 	 * 2. Append "News" and "Views" menu items to the end.
 	 * @param {Array<Object>} sitePages - All menu pages sorted by menu_order.
 	 * @param {Boolean} includeHomeItem - A flag indicating if the home menu item should be included.
-	 * @return {Array<Object>} An array of object including the slug, menu title and href.
+	 * @return {Array<menuItem>} An array of object including the slug, menu title and href.
 	 */
 	generateNavMenu (sitePages, includeHomeItem) {
-		// Don't include the menu item for "home" by default
-		includeHomeItem = includeHomeItem === undefined ? false : includeHomeItem;
 		const menuItems = [];
 		const homeMenuItem = [];
 		const itemsToAppend = [{
@@ -30,7 +36,7 @@ export default {
 		}];
 
 		sitePages.forEach((oneSitePage) => {
-			if (includeHomeItem && oneSitePage.slug === "home") {
+			if (includeHomeItem && oneSitePage.slug === "home" && oneSitePage.menu_order === 0) {
 				homeMenuItem.push({
 					slug: oneSitePage.slug,
 					title: "Home",
@@ -38,7 +44,8 @@ export default {
 				});
 			}
 
-			if (oneSitePage.menu_order > 0) {
+			// Exclude pages that have menu_order === 0, which indicates the page should not be displayed on the header nav menu.
+			if (oneSitePage.menu_order) {
 				menuItems.push({
 					slug: oneSitePage.slug,
 					title: oneSitePage.title,
