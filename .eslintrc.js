8eb33ba996c249cd0a99ae79e311da8f5f742090
Cindy Qi Li <cli@ocadu.ca>
2020-05-02 11:44:41 -0400

    fix: fix indentations in hidden files intentionally ignored by eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-  env: {
-    es6: true,
+	env: {
+		es6: true,
 		node: true,
 		browser: true
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2017
+	},
+	extends: "eslint:recommended",
+	parserOptions: {
+		sourceType: "module",
+		ecmaVersion: 2017
 	},
 	"ignorePatterns": ["src/_includes/static/", "dist"],
-  rules: {
+	rules: {
 		"indent": [1, "tab"],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"]
-  }
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"]
+	}
 };
