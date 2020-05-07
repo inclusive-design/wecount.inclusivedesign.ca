5eb3c714ee5f43d48ef728ef3cfaa725dd02b05d
TeddTech <ted_thompson@live.com>
2020-05-07 09:55:19 -0700

    fix: add uio

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,8 @@ mix.js("./src/js/utils.js", "./src/_includes/static/js/utils.js");
 mix.js("./src/js/search.js", "./src/_includes/static/js/search.js");
 
 mix.copyDirectory("./src/fonts", "./src/_includes/static/fonts")
-	.copyDirectory("./src/images", "./src/_includes/static/images");
+	.copyDirectory("./src/images", "./src/_includes/static/images")
+	.copyDirectory("./infusion", "./src/_includes/static/infusion");
 
 mix.options({
 	processCssUrls: false,
