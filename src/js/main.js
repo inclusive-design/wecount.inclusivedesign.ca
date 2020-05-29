3c64140b351d39e34a8261ec34d2a351507169bc
Cindy Qi Li <cli@ocadu.ca>
2020-05-29 17:18:43 -0400

    fix: fix issues with the dropdown menu on the mobile layout

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,8 @@ document.addEventListener("click", (event) => {
 	if (event.target.id === "menuToggleButton") {
 		const expanded = "true" === event.target.getAttribute( "aria-expanded" ) || false;
 		event.target.setAttribute( "aria-expanded", !expanded );
+	} else {
+		// click anywhere else on the page closes the pop up menu
+		document.getElementById("menuToggleButton").setAttribute( "aria-expanded", false );
 	}
 });
