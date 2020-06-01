371d67b77e3770ae9a75fb6bc8f74f13ea1e3c37
Tedd Tech <ted_thompson@live.com>
2020-06-01 06:51:46 -0700

    Add uio (#230)
    * fix: add uio
    
    * fix: removed infusion folder at root level
    
    * fix: infusion npm implementation
    
    * fix: start figma styling of uio tab
    
    * fix: applied figma styling to uio tap using important
    
    * fix: add stylelint workaround
    
    * fix: uio tab styling specificity
    
    * fix: remove uio toc styling
    
    * fix: add roboto font on uio tab
    
    * fix: implement cindys uiowecount infusion branch
    
    * fix: used preload value on link tags for performance improvement
    
    * fix: redo remove text size panel from uio options
    
    * fix: point infusion to 3.0.0-dev.20200513T135321Z.3ebc41052.FLUID-6145
    
    * fix: updated infusion v to dev.20200514T192349Z.6ec8e0fcf.UIO-WeCount
    
    * fix: updated package.lock to reflect infustion update
    
    * fix: added swap value to font display for font-faces
     add preload for infusion uio min js script
    
    * fix: properly format basenjk using tabs
    
    * fix: preload fonts
    
    * fix: added link stylsheet elements for css files
    
    * fix: formated base njk
    
    * fix: added lighthouse performance comments
    
    removed font preloads
    
    * fix: add latest infusion version publication
    
    * fix: hover and focus stlying to uio tab
    
    changed font display swap to optional
    
    added syllabes to uio preference panel
    
    * fix: remove comments from uio tab hover and focus styling
    
    * fix: font test
    
    * fix: font display fallback
    
    * fix: highlight uio tab on hover and focus
    
    * fix: test use of font display fallback
    
    * fix: removed ignorefortoc
    
    * fix: remove bolded outline from uio tab on hover and focus
    
    * fix: remove uio syllabification code
    
    * fix: added preloads for we count fonts

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,8 @@ mix.js("./src/js/utils.js", "./src/_includes/static/js/utils.js");
 mix.js("./src/js/search.js", "./src/_includes/static/js/search.js");
 
 mix.copyDirectory("./src/fonts", "./src/_includes/static/fonts")
-	.copyDirectory("./src/images", "./src/_includes/static/images");
+	.copyDirectory("./src/images", "./src/_includes/static/images")
+	.copyDirectory("./node_modules/infusion", "./src/_includes/static/lib/infusion");
 
 mix.options({
 	processCssUrls: false,
