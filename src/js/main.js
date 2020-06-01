116a88188d7aec8a67e0bd367b40e4ca745b69ff
Cindy Qi Li <cli@ocadu.ca>
2020-06-01 14:23:33 -0400

    fix: fix the javascript for the search feature on the mobile layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+// Clicking the menu toggle button on the mobile layout opens up the popup menu.
+// Clicking anywhere else closes this popup menu.
 document.addEventListener("click", (event) => {
 	if (event.target.id === "menuToggleButton") {
 		const expanded = "true" === event.target.getAttribute( "aria-expanded" ) || false;
@@ -6,4 +8,10 @@ document.addEventListener("click", (event) => {
 		// click anywhere else on the page closes the pop up menu
 		document.getElementById("menuToggleButton").setAttribute( "aria-expanded", false );
 	}
+});
+
+// Clicking the search svg on the header enlarges the search input box and places the focus in it.
+// This is required for the mobile layout where the search icon overlays on top of the input field.
+document.querySelector(".site-nav .search-container svg").addEventListener("click", () => {
+	document.querySelector(".site-nav .search-container #search-form input").focus();
 });
