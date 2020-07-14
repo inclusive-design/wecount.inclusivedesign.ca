e3ee95126f972c27657e8fbb9a53492d146d9fbd
Cindy Qi Li <cli@ocadu.ca>
2020-07-14 15:21:46 -0400

    fix: implement the filter UI changes

--- src/js/views-dynamic-handler.js
+++ src/js/views-dynamic-handler.js
@@ -77,10 +77,27 @@ new Vue({
 	computed: {}
 });
 
+/*
+ * Show/hide the corresponding arrow up and down buttons based on the expand state
+ * @param {DOM element} expandButtonElm - The DOM element of the expand button.
+ * @param {String} expandButtonState - A value of "true" or "false".
+ */
+function setExpandSVGState(expandButtonElm, expandButtonState) {
+	const arrowupSVG = $(expandButtonElm).find(".arrowup");
+	arrowupSVG[expandButtonState === "false" ? "hide" : "show"]();
+	const arrowdownSVG = $(expandButtonElm).find(".arrowdown");
+	arrowdownSVG[expandButtonState === "true" ? "hide" : "show"]();
+}
+
 // Clicking the expand button on the filter header opens/closes the filter
 const expandButtons = document.querySelectorAll(".filter .filter-expand-button");
 
 for (let i = 0; i < expandButtons.length; i++) {
+	// At the page load, show/hidearrow up and down buttons based on the aria-expand state of the expand button
+	const initialExpandedValue = expandButtons[i].getAttribute("aria-expanded");
+	setExpandSVGState(expandButtons[i], initialExpandedValue);
+
+	// Add event listener for expand buttons
 	expandButtons[i].addEventListener("click", (e) => {
 		e.preventDefault();
 		const currentExpandedValue = expandButtons[i].getAttribute("aria-expanded");
@@ -93,6 +110,9 @@ for (let i = 0; i < expandButtons.length; i++) {
 		// on one of expand buttons only opens the form that this button corresponds to.
 		const filter = $(expandButtons[i]).parent().siblings();
 		filter[expandedState === "false" ? "hide" : "show"]();
+
+		// Show/hide the expand svg
+		setExpandSVGState(expandButtons[i], expandedState);
 	});
 }
 
