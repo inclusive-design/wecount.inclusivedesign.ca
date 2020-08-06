b9428ac8590682043aaae42fac86f6a98ca22ba5
Cindy Qi Li <cindyli@users.noreply.github.com>
2020-08-06 11:51:11 -0400

    Feat: Implement the Initiatives page by fetching data from Airtable (resolves #349) (#367)

---
+++ src/utils/data-fetcher-airtable.js
@@ -0,0 +1,52 @@
+// Communicate with Airtable APIs to access data
+const env = require("../_data/env");
+const airtable = require("airtable");
+var md = require("markdown-it")({
+	breaks: true
+});
+
+airtable.configure({
+	apiKey: process.env.AIRTABLE_API_KEY
+});
+
+var base = airtable.base(env.airtableBase);
+
+// Share data fetch functions
+module.exports = {
+	workshops: async () => {
+		return new Promise((resolve) => {
+			const workshops = [];
+
+			base("workshops").select({
+				view: "Grid view",
+				sort: [{field: "date", direction: "desc"}]
+			}).eachPage(function page(records, fetchNextPage) {
+				records.forEach(record => {
+					let image = record.get("image");
+					let title = record.get("title");
+					let shortDescription = record.get("short_description");
+					let fullDescription = record.get("full_description");
+
+					workshops.push({
+						id: record.getId(),
+						title: title ? md.render(title) : undefined,
+						date: record.get("date"),
+						shortDescription: shortDescription ? md.render(shortDescription) : undefined,
+						fullDescription: fullDescription ? md.render(fullDescription) : undefined,
+						imageUrl: image ? image[0].url : undefined,
+						imageAlt: record.get("image_alt")
+					});
+				});
+				fetchNextPage();
+			}, function done(err) {
+				if (err) {
+					console.error("Error reading 'workshops' table from Airtable: ", err);
+					return;
+				}
+				else {
+					resolve(workshops);
+				}
+			});
+		});
+	}
+};
