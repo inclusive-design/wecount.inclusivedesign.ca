4898ade4c4be0e68c96e6fd86bba20999f6028b5
Cindy Qi Li <cli@ocadu.ca>
2020-08-20 14:41:38 -0400

    fix: remove debugging info

--- functions/comments.js
+++ functions/comments.js
@@ -45,8 +45,6 @@ const sendEmail = ({ timestamp, name, comment }) => {
 				console.error(error);
 				return reject(error);
 			} else {
-        console.log("Email sent from: " + process.env.EMAIL_FROM);
-        console.log("Email sent to: " + process.env.EMAIL_TO);
 				console.log("Email sent: " + info.response);
 				resolve();
 			}
