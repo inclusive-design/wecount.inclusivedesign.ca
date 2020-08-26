3b30ee39813b6ce27fda12bdc91e2008ec9621d0
TeddTech <ted_thompson@live.com>
2020-08-26 11:58:21 -0700

    feat: added workshop page

---
+++ src/js/workshop-comments.js
@@ -0,0 +1,80 @@
+const form = document.getElementById("comment-form");
+
+const name = document.getElementById("name");
+const comment = document.getElementById("comment");
+console.log("Is this working???????");
+
+form.addEventListener("submit", (event) => {
+	console.log("HELLLLLLP");
+	event.preventDefault();
+
+	if (setRequiredMessage(name) & setRequiredMessage(comment)) {
+
+		const XHR = new XMLHttpRequest();
+
+		// Bind the FormData object and the form element
+		const FD = new FormData( form );
+
+		// Define what happens on successful data submission
+		XHR.addEventListener( "load", function() {
+			setSubmittedComment();
+		} );
+
+		// Define what happens in case of error
+		XHR.addEventListener( "error", function() {
+			alert( "Oops! Something went wrong. Sorry Your comment was not submitted." );
+		} );
+
+		// Set up our request
+		console.log(window.location.origin);
+		XHR.open( "POST", window.location.origin + "/api/comments" );
+
+		// The data sent is what the user provided in the form
+		console.log(FD);
+		XHR.send( FD );
+
+
+	}
+});
+
+function setRequiredMessage(input){
+
+	if (input.value.trim() === "") {
+		// show required message
+		const formControl = input.parentElement;
+		const requiredMessage = formControl.querySelector(".required");
+		requiredMessage.setAttribute("style", "display: inline;");
+
+		return false;
+
+	} else {
+		const formControl = input.parentElement;
+		const requiredMessage = formControl.querySelector(".required");
+		requiredMessage.setAttribute("style", "display: none;");
+
+		return true;
+	}
+}
+
+function setSubmittedComment() {
+
+	const submittedComment = document.querySelector(".submitted-comment");
+
+	const submittedCommentDate = submittedComment.querySelector(".comment-date");
+	const submittedCommentName = submittedComment.querySelector(".comment-name");
+	const submittedCommentComment = submittedComment.querySelector(".comment-comment");
+
+	const dateObject = new Date();
+	const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	submittedCommentDate.innerText = `${months[dateObject.getMonth()]} ${dateObject.getDate()}, ${dateObject.getFullYear()}`;
+
+	submittedCommentName.innerText = name.value.trim();
+	submittedCommentComment.innerText = comment.value.trim();
+
+
+	const submittedCommentMessage = document.querySelector(".submitted-comment-message");
+	submittedCommentMessage.setAttribute("style", "display: block;");
+
+	submittedComment.setAttribute("style", "display: block;");
+
+}
--- src/utils/data-fetcher-airtable.js
+++ src/utils/data-fetcher-airtable.js
@@ -26,15 +26,41 @@ module.exports = {
 					let title = record.get("title");
 					let shortDescription = record.get("short_description");
 					let fullDescription = record.get("full_description");
+					let comments = [];
+
+					let workshopFilterFormula = "{recID (from workshops)} = '" + record.id + "'";
+
+					base("comments").select({
+						view: "reviewed",
+						sort: [{field: "post_date", direction: "desc"}],
+						filterByFormula: workshopFilterFormula
+					}).eachPage(function page(recs, fetchNextPage) {
+
+						recs.forEach(function(rec) {
+							let commentProps = {
+								name: rec.get("name"),
+								date: rec.get("post_date"),
+								comment: rec.get("comment")
+							};
+							comments.push(commentProps);
+						});
+
+						fetchNextPage();
+					}, function done(err) {
+						if (err) {
+							console.error("Error reading 'comments' table from Airtable: ", err);
+							return; }
+					});
 
 					workshops.push({
 						id: record.getId(),
-						title: title ? md.render(title) : undefined,
+						title: title ? title : undefined,
 						date: record.get("date"),
 						shortDescription: shortDescription ? md.render(shortDescription) : undefined,
 						fullDescription: fullDescription ? md.render(fullDescription) : undefined,
 						imageUrl: image ? image[0].url : undefined,
-						imageAlt: record.get("image_alt")
+						imageAlt: record.get("image_alt"),
+						comments
 					});
 				});
 				fetchNextPage();
