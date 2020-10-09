4b031e899daaa8933dbc7a6ce9614050021b8acb
Cindy Qi Li <cli@ocadu.ca>
2020-10-09 16:50:44 -0400

    feat: add the advisory panel page

---
+++ src/js/advisory-panel-handler.js
@@ -0,0 +1,45 @@
+// For the Advisory Panel page
+
+/* global $ */
+
+// Toggle the bio description that associates with the clicked expand link.
+// Also adjust the aria attributes of the link correspondingly.
+function toggleExpandState(expandLink) {
+	const currentExpandedValue = expandLink.getAttribute("aria-expanded");
+	const expandedState = currentExpandedValue === "true" ? "false" : "true";
+	expandLink.setAttribute("aria-expanded", expandedState);
+	expandLink.setAttribute("aria-label", expandedState === "true" ? "collapse" : "expand");
+
+	// Toggle the bio description
+	const bioDesc = $(expandLink).closest(".wp-block-media-text").siblings("p");
+	bioDesc[expandedState === "false" ? "hide" : "show"]();
+}
+
+$(document).ready(function () {
+	// Set all bio descption paragraphs as live regions
+	$(document.querySelectorAll(".wp-block-media-text")).siblings("p").attr("aria-live", "polite");
+
+	// Clicking the expand link opens/closes the corresponding bio description
+	const expandLinks = document.querySelectorAll(".wp-block-button__link");
+
+	for (let i = 0; i < expandLinks.length; i++) {
+		// At the page load, set proper ARIA attributes
+		expandLinks[i].setAttribute("aria-label", "expand");
+		expandLinks[i].setAttribute("aria-expanded", "false");
+		expandLinks[i].setAttribute("tabindex", "0");
+
+		// Add event listener for expand links. Clicking the link or hitting the enter key when focusing
+		// on the link triggers the toggle.
+		expandLinks[i].addEventListener("click", (e) => {
+			e.preventDefault();
+			toggleExpandState(expandLinks[i]);
+		});
+
+		expandLinks[i].addEventListener("keypress", (e) => {
+			// Hitting the enter key also triggers the toggle.
+			if (e.which === 13) {
+				toggleExpandState(expandLinks[i]);
+			}
+		});
+	}
+});
