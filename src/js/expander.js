fa22b8a1d8a69bef962fee7282726c57b24919dd
Ned Zimmerman <ned@bight.dev>
2021-04-08 14:51:37 -0300

    fix: adjust click listener for expander based on feedback

--- src/js/expander.js
+++ src/js/expander.js
@@ -1,12 +1,14 @@
-document.addEventListener("click", event => {
-	if (!event.target.closest(".expander button")) return false;
-	const button = event.target.closest(".expander button");
-	const expanded = button.getAttribute("aria-expanded") === "true" || false;
-	const content = button.parentNode.nextElementSibling;
-	button.setAttribute("aria-expanded", !expanded);
-	if (expanded) {
-		content.setAttribute("hidden", "true");
-	} else {
-		content.removeAttribute("hidden");
-	}
+window.addEventListener("load", () => {
+	document.querySelectorAll(".expander button").forEach(button => {
+		button.addEventListener("click", () => {
+			const expanded = button.getAttribute("aria-expanded") === "true" || false;
+			const content = button.parentNode.nextElementSibling;
+			button.setAttribute("aria-expanded", !expanded);
+			if (expanded) {
+				content.setAttribute("hidden", "true");
+			} else {
+				content.removeAttribute("hidden");
+			}
+		});
+	});
 });
