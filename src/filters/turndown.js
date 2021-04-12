76d9fce1568e4031a6022a21c5045a15fbc74acd
Ned Zimmerman <ned@bight.dev>
2021-04-12 11:39:00 -0300

    chore: expand turndown config to migrate other content

--- src/filters/turndown.js
+++ src/filters/turndown.js
@@ -5,5 +5,39 @@ module.exports = value => {
 		headingStyle: "atx",
 		codeBlockStyle: "fenced"
 	});
+
+	td.addRule("youtube", {
+		filter: ["iframe"],
+		replacement: (innerHTML, node) => {
+			return `{% youtube "${node.getAttribute("src").replace("?feature=oembed", "")}" %}`;
+		}
+	});
+
+	td.addRule("image-and-text", {
+		filter: (node) => {
+			return node.classList.contains("wp-block-media-text");
+		},
+		replacement: (innerHTML, node) => {
+			let contentEl, content, img, image, alt, imagePosition;
+			contentEl = node.querySelector(".wp-block-media-text__content");
+			if (contentEl) {
+				content = td.turndown(contentEl.innerHTML);
+			} else {
+				content = "";
+			}
+			img = node.querySelector("img");
+			image = img.getAttribute("src");
+			alt = img.getAttribute("alt");
+			imagePosition = node.classList.contains("has-media-on-the-right") ? "right" : "left";
+			return `{% imageAndText "${image}", "${alt}", "${imagePosition}", "top" %}
+
+${content}
+
+{% endimageAndText %}
+
+`;
+		}
+	});
+
 	return td.turndown(value);
 };
