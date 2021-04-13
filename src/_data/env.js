bd163e08a073873492e1bbea92481dba3084d002
Ned Zimmerman <ned@bight.dev>
2021-04-13 09:17:42 -0300

    chore: remove WordPress data fetcher and environment variable

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 module.exports = {
-	wpApi: process.env.CONTEXT === "production" ? "https://wecount-cms.inclusivedesign.ca/wp-json/wp/v2" : "https://wecount-dev.inclusivedesign.ca/wp-json/wp/v2",
 	airtableBase: process.env.CONTEXT === "production" ? process.env.AIRTABLE_BASE_PRODUCTION : process.env.AIRTABLE_BASE_DEV,
 	context: process.env.CONTEXT || "dev",
 	baseUrl: process.env.DEPLOY_PRIME_URL || false
