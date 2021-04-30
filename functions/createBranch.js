5db9d46df57654c1312f03b7b30605c7c88c5411
Cindy Qi Li <cli@ocadu.ca>
2021-04-30 13:43:30 -0400

    test: a simple operation with /tmp directory

--- functions/createBranch.js
+++ functions/createBranch.js
@@ -8,14 +8,14 @@
  */
 
 const fs = require("fs");
-const rimraf = require("rimraf");
-const utils = require("./utils.js");
+// const rimraf = require("rimraf");
+// const utils = require("./utils.js");
 
 // The name of the temporary local directory for cloning the covid data repo locally
 const clonedLocalDir = "/tmp";
-const dataRepoUrl = "https://github.com/cindyli/inverted-wordles";
-const branchName = "from-netlify";
-const repoUrl = process.env.GITHUB_ACCOUNT_URL;
+// const dataRepoUrl = "https://github.com/cindyli/inverted-wordles";
+// const branchName = "from-netlify";
+// const repoUrl = process.env.GITHUB_ACCOUNT_URL;
 
 exports.handler = async function(event, context, callback) {
 	if (event.httpMethod !== "POST") {
@@ -24,23 +24,23 @@ exports.handler = async function(event, context, callback) {
 			body: "Only accept POST requests."
 		});
 	} else {
-		console.log("Cloning repository...");
-		await utils.prepareLocalRepo(dataRepoUrl, clonedLocalDir, repoUrl);
-
+		// console.log("Cloning repository...");
+		// await utils.prepareLocalRepo(dataRepoUrl, clonedLocalDir, repoUrl);
+		//
 		console.log("Creating a test file...");
 		fs.writeFileSync(clonedLocalDir + "/test.txt", "{a: \"b\"}");
 
-		console.log("Pushing updated files to a remote branch...");
-		await utils.createRemoteBranch(branchName, clonedLocalDir);
-
-		console.log("Cleaning up the cloned directory...");
-		rimraf.sync(clonedLocalDir);
+		// console.log("Pushing updated files to a remote branch...");
+		// await utils.createRemoteBranch(branchName, clonedLocalDir);
+		//
+		// console.log("Cleaning up the cloned directory...");
+		// rimraf.sync(clonedLocalDir);
 
 		console.log("Done!");
 
 		callback(null, {
 			statusCode: 200,
-			body: "Done!"
+			body: fs.readFileSync(clonedLocalDir + "/test.txt")
 		});
 	}
 };
