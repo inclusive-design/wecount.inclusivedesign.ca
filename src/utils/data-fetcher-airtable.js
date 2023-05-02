1c680a502a0830535f1491951fde2e311aed6a37
Daniel Cho <chosww@gmail.com>
2023-05-02 13:28:22 -0400

    chore: remove airtable resource fetch migration

--- src/utils/data-fetcher-airtable.js
+++ src/utils/data-fetcher-airtable.js
@@ -1,7 +1,6 @@
 // Communicate with Airtable APIs to access data
 const env = require("../_data/env");
 const airtable = require("airtable");
-const learnTags = require("../_data/resourceTags.json");
 
 airtable.configure({
 	apiKey: process.env.AIRTABLE_API_KEY
@@ -38,47 +37,6 @@ module.exports = {
 					return;
 				} else {
 					resolve(comments);
-				}
-			});
-		});
-	},
-	populateResources: async () => {
-		return new Promise((resolve, reject) => {
-			const tagLabelToValue = {};
-			learnTags.resourceTags.forEach(tag => {
-				tagLabelToValue[tag.label] = tag.value;
-			});
-			const resources = [];
-			// You can find table id from the airtable url, a part where it begins with 'tbl'
-			base("tbll2NpygbXrUnZZV").select().eachPage(function page(records, fetchNextPage) {
-				records.forEach(function(record) {
-					let resource = {
-						title: record.get("title"),
-						focus: record.get("focus"),
-						summary: record.get("summary") ? record.get("summary").replace(/\n/g, " ") : "",
-						learnTags: [],
-						source: record.get("source"),
-						keywords: [],
-						type: record.get("resourceType"),
-						link: record.get("url"),
-						openSource: true,
-						jurisdiction: record.get("jurisdiction")
-					};
-					record.get("learnTag").forEach(learnTag => {
-						if (tagLabelToValue[learnTag]) {
-							resource.learnTags.push(tagLabelToValue[learnTag]);
-						}
-					});
-					resources.push(resource);
-				});
-
-				fetchNextPage();
-			}, function done(err) {
-				if (err) {
-					console.error("Error reading resources table from Airtable:");
-					reject(err);
-				} else {
-					resolve(resources);
 				}
 			});
 		});
