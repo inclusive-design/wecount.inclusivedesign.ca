13b204ea2884d145a788ee0faffa7bdf5046cbb8
Ned Zimmerman <ned@bight.dev>
2024-02-07 11:56:46 -0400

    feat: use lite-youtube-embed for YouTube videos (#1684)

--- src/shortcodes/youtube.js
+++ src/shortcodes/youtube.js
@@ -4,6 +4,6 @@ module.exports = (url) => {
 	const id = getId(url);
 
 	if (id) {
-		return `<figure class="embed--youtube"><iframe class="youtube-player video video--youtube" src="https://www.youtube.com/embed/${id}/" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></figure>`;
+		return `<figure class="embed--youtube"><lite-youtube videoid="${id}" style="background-image: url('https://i.ytimg.com/vi/${id}/hqdefault.jpg');"><a href="https://youtube.com/watch?v=${id}" class="lty-playbtn"><span class="lyt-visually-hidden">Play Video</span></a></lite-youtube></figure>`;
 	}
 };
