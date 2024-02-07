ddfcb1a42ea42c03a6b4fd22c21645a15b02fc70
Daniel <chosww@gmail.com>
2024-02-07 10:37:43 -0500

    feat: add support for image captions (resolves #1670) (#1683)

--- src/shortcodes/image-and-text.js
+++ src/shortcodes/image-and-text.js
@@ -1,8 +1,10 @@
 const markdownFilter = require("../filters/markdown.js");
+const figureShortcode = require("../../node_modules/eleventy-plugin-fluid/src/shortcodes/figure-shortcode.js");
 
-module.exports = (content, image, alt, imagePosition, verticalAlignment) => {
+module.exports = (content, image, alt, caption, imagePosition, verticalAlignment) => {
+	const figure = caption ? figureShortcode(caption, image, alt) : figureShortcode("", image, alt);
 	return `<div class="image-and-text image-and-text--image-${imagePosition} image-and-text--vertical-${verticalAlignment}">
-<figure class="image-and-text__media"><img src="${image}" alt="${alt}" /></figure>
+	${figure}
 <div class="image-and-text__content">${markdownFilter(content)}</div>
 </div>`;
 };
