84ec8ab10f4481c40f9b652a1160ce65ce5e3c81
Ned Zimmerman <ned@bight.dev>
2024-08-02 13:29:31 -0300

    feat: remove comments (resolves #1847)

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 module.exports = {
-	airtableBase: process.env.CONTEXT === "production" ? process.env.AIRTABLE_BASE_PRODUCTION : process.env.AIRTABLE_BASE_DEV,
 	context: process.env.CONTEXT || "dev",
 	baseUrl: process.env.DEPLOY_PRIME_URL || false
 };
