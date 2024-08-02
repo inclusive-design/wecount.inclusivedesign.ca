3f2ce2972bf5e9790c46dcf1e0f6f5540a39b09b
Ned Zimmerman <ned@bight.dev>
2024-08-02 15:10:47 -0300

    feat: group RTDD/RDDN in nav (resolves #1850)

--- src/collections/pages/pages.11tydata.js
+++ src/collections/pages/pages.11tydata.js
@@ -5,6 +5,7 @@ module.exports = {
 		bannerImage: data => data.bannerImage ? optimizeImage(data.bannerImage) : false,
 		eleventyNavigation: {
 			key: data => data.key ? data.key : data.title,
+			translation_key: data => data.translation_key ? data.translation_key : null,
 			parent: data => data.parent ? data.parent : false,
 			order: data => data.menu_order ? data.menu_order : false,
 			locale: data => data.locale ? data.locale : "en-CA"
