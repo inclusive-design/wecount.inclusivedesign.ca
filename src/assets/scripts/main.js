a3196077676a31a54b7f3e1da7cada8a0a3ec9b3
Ned Zimmerman <ned@bight.dev>
2024-08-12 14:00:52 -0300

    feat: refactoring [WIP]

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,36 +1,13 @@
-/* global toggleMenu */
+// Clicking the menu toggle button opens up the disclosure menu.
+// Clicking anywhere else closes the disclosure menu.
+document.addEventListener("click", (event) => {
+	if (!event.target.closest(".site-nav .primary-nav") && !event.target.closest(".site-nav [aria-expanded]") && !event.target.matches(".site-nav [aria-expanded]")) {
+		const toggleButton = document.querySelector(".site-nav [aria-expanded]");
+		toggleButton.setAttribute("aria-expanded", false);
+		return;
+	};
 
-const toggleButton = document.getElementById("menuToggleButton");
-toggleButton.setAttribute("aria-expanded", false);
-$("div.primary-nav").toggle(false);
-
-// Clicking the menu toggle button on the mobile layout opens up the popup menu
-// and sets the button "aria-expanded" attribute value properly.
-// eslint-disable-next-line
-toggleMenu = function (expanded) {
-	document.getElementById("menuToggleButton").setAttribute("aria-expanded", expanded);
-	$("div.primary-nav").toggle(expanded);
-};
-
-// Clicking the menu toggle button on the mobile layout opens up the popup menu.
-// Clicking anywhere else closes this popup menu.
-toggleButton.addEventListener("click", (event) => {
-	const expanded = "true" === event.target.getAttribute("aria-expanded") || false;
-	toggleMenu(!expanded);
-});
-
-// document.addEventListener("click", (event) => {
-// 	if (event.target.id === "menuToggleButton") {
-// 		const expanded = "true" === event.target.getAttribute("aria-expanded") || false;
-// 		toggleMenu(!expanded);
-// 	} else {
-// 		// click anywhere else on the page closes the pop up menu.
-// 		toggleMenu(false);
-// 	}
-// });
-
-// Clicking the search svg on the header enlarges the search input box and places the focus in it.
-// This is required for the mobile layout where the search icon overlays on top of the input field.
-document.querySelector(".site-nav .search-container svg").addEventListener("click", () => {
-	document.querySelector(".site-nav .search-container input").focus();
+	const toggleButton = event.target.closest(".site-nav [aria-expanded]");
+	const expanded = "true" === toggleButton.getAttribute("aria-expanded") || false;
+	toggleButton.setAttribute("aria-expanded", !expanded);
 });
